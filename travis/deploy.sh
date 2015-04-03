4ce9bb45b290604dc87d611fcd2e820cf8ab6a28
Hajime Morrita <omo@dodgson.org>
2015-04-02 20:52:22 -0700

    Tell s2pd to clear the cache.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Note: You need some env vars to make this work.
 aws s3 sync --acl=public-read --delete --storage-class=REDUCED_REDUNDANCY public/ s3://anemone.dodgson.org
+curl -d "" http://steps.dodgson.org/clear
