241a03b6e6024435eaf812265d7f57e3facb5d1f
Hajime Morrita <morrita@flakiness.es>
2017-02-18 18:05:12 -0800

    Fix broken build.

--- travis/install.sh
+++ travis/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pip install awscli
 mkdir -p out
-cd out && tar xvf hugo.tgz
 curl -L https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz > out/hugo.tgz
+cd out && tar xvf hugo.tgz
