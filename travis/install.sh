416613578ae16feff54971c4d5bd154081bfaacc
Hajime Morrita <morrita@flakiness.es>
2017-02-18 17:34:27 -0800

    Travis: Update hugo version.

--- travis/install.sh
+++ travis/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pip install awscli
 mkdir -p out
-curl -L https://github.com/spf13/hugo/releases/download/v0.13/hugo_0.13_linux_amd64.tar.gz > out/hugo.tgz
 cd out && tar xvf hugo.tgz
+curl -L https://github.com/spf13/hugo/releases/download/v0.18.1/hugo_0.18.1_Linux-64bit.tar.gz > out/hugo.tgz
