d571e113ccf934295b38a96f8d84a62c924c79c0
Hajime Morrita <omo@dodgson.org>
2019-12-08 08:09:59 -0800

    Update theme commits.
    Along with hugo version update.

--- travis/install.sh
+++ travis/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 pip install awscli
 mkdir -p out
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.31.1/hugo_0.31.1_Linux-64bit.tar.gz > out/hugo.tgz
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.60.1/hugo_0.60.1_Linux-64bit.tar.gz > out/hugo.tgz
 cd out && tar xvf hugo.tgz
